5a94482012b70b7da55af2179e37d8f8e1707b3f
Nick Hortovanyi <nick.hortovanyi@gmail.com>
2017-06-28 12:37:11 +1000

    turret rotating with barrel moving

--- BattleTank/Source/BattleTank/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/TankAimingComponent.cpp
@@ -8,7 +8,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
@@ -94,5 +94,6 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
     
 //    UE_LOG(LogTemp, Warning, TEXT("AimAsRotator: %s"), *AimAsRotator.ToString())
     
-    Barrel->Elevate(DeltaRotator.Pitch);    
+    Barrel->Elevate(DeltaRotator.Pitch);
+    Turret->Rotate(DeltaRotator.Yaw);
 }
