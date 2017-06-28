68a47dd78c366a3867ab4939b3b9967759ceffde
Nick Hortovanyi <nick.hortovanyi@gmail.com>
2017-06-28 15:39:18 +1000

    projectile firing :)

--- BattleTank/Source/BattleTank/Projectile.h
+++ BattleTank/Source/BattleTank/Projectile.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Runtime/Engine/Classes/GameFramework/ProjectileMovementComponent.h"
 #include "GameFramework/Actor.h"
 #include "Projectile.generated.h"
 
@@ -23,6 +24,9 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
+    void LaunchProjectile(float Speed);
+    
+private:
+    UProjectileMovementComponent* ProjectileMovement = nullptr;
 	
 };
--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -29,17 +29,16 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 void ATank::Fire() {
     if (!Barrel) { return ;}
     
-    auto Time = GetWorld()->GetTimeSeconds();
-    UE_LOG(LogTemp, Warning, TEXT("%f: Tank fire"), Time);
-    
-    
     // Spawn a projectile at the socket location on the barrel
-    GetWorld()->SpawnActor<AProjectile>
+    auto Projectile = GetWorld()->SpawnActor<AProjectile>
     (
      ProjectileBlueprint,
      Barrel->GetSocketLocation(FName("Projectile")),
      Barrel->GetSocketRotation(FName("Projectile"))
     );
+    
+    Projectile->LaunchProjectile(LaunchSpeed);
+    
 }
 
 void ATank::AimAt(FVector HitLocation)
